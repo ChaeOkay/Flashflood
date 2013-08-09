be58c82bfb8826ef561f3a8b0f6c127bf1c587b7
Apprentice <apprentice@dbc15.local>
2013-08-09 14:25:54 -0700

    controller outline

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,34 @@
 get '/' do
-  # Look in app/views/index.erb
-  erb :index
+  # Look in app/views/index.erb 
+  erb :sign_in
+end
+
+get '/sign_up' do
+
+  erb :sign_up
+end
+
+post '/sign_up' do
+
+  erb :dashboard
+end
+
+get '/:user_id/dashboard' do
+
+  erb :dashboard
+end
+
+get '/:user_id/deck/:id/:card_id' do
+
+  erb :game
+end
+
+post '/:user_id/deck/:id/:card_id' do
+
+  erb :game
+end
+
+get '/logout' do
+
+  erb :logout
 end
