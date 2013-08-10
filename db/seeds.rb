9779c7013efeda87f86b8b7166b970f81b4035e7
DBC-Apprentice <dbc@dbc.com>
2013-08-09 17:03:12 -0700

    presentation 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
 # martino = User.create(username: 'Martino')
 # martino.password= 'crazy'
 # martino.save
-# state_capitals = Deck.create(name: 'State Capitals')
+state_capitals = Deck.find_by_name('State Capitals')
 
-# ca = Card.create(question: 'What is the capital of California?', answer: 'Sacramento')
-# mi = Card.create(question: 'What is the capital of Michigan?', answer: 'Lansing')
-# pa = Card.create(question: 'What is the capital of Pennsyvania?', answer: 'Harrisburg')
+ca = Card.create(question: 'What is the capital of California?', answer: 'Sacramento')
+mi = Card.create(question: 'What is the capital of Michigan?', answer: 'Lansing')
+pa = Card.create(question: 'What is the capital of Pennsyvania?', answer: 'Harrisburg')
 
 
-# state_capitals.cards.create(question: 'What is the capital of California?', answer: 'Sacramento')
-# state_capitals.cards << mi
-# state_capitals.cards << pa
+state_capitals.cards.create(question: 'What is the capital of California?', answer: 'Sacramento')
+state_capitals.cards << mi
+state_capitals.cards << pa
 
 # martino.decks << state_capitals
 
@@ -19,5 +19,7 @@ round = Round.where(user_id: martino[:id], deck_id: state_capitals[:id])
 # round.num_incorrect = 3
 
 
+
 # country_capitals = Deck.create(name: 'Country Capitals')
+
 # Deck.find_by_name('Deck Capitals').destroy
